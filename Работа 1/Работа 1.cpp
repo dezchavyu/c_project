43418acb6129569569ae40b571f1a92a30a5acbe
dazchavyu <justdenyt06@gmail.com>
2021-11-24 19:57:30 +0300

    projects

---
+++ Работа 1/Работа 1.cpp
@@ -0,0 +1,177 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+
+
+    int a1 = 1;
+    int a2 = 2;
+    int a3 = 3;
+    int a4 = 4;
+    int a5 = 5;
+    int a6 = 6;
+    int a7 = 7;
+    int a8 = 8;
+    int a9 = 9;
+    int a10 = 10;
+
+    cout << "a1 = " << a1 << " | тип: int, размер: 4б \n";
+    cout << "a2 = " << a2 << " | тип: int, размер: 4б \n";
+    cout << "a3 = " << a3 << " | тип: int, размер: 4б \n";
+    cout << "a4 = " << a4 << " | тип: int, размер: 4б \n";
+    cout << "a5 = " << a5 << " | тип: int, размер: 4б \n";
+    cout << "a6 = " << a6 << " | тип: int, размер: 4б \n";
+    cout << "a7 = " << a7 << " | тип: int, размер: 4б \n";
+    cout << "a8 = " << a8 << " | тип: int, размер: 4б \n";
+    cout << "a9 = " << a9 << " | тип: int, размер: 4б \n";
+    cout << "a10 = " << a10 << " | тип: int, размер: 4б \n";
+
+    cout << "\n";
+
+    short b1 = 1;
+    short b2 = 2;
+    short b3 = 3;
+    short b4 = 4;
+    short b5 = 5;
+    short b6 = 6;
+    short b7 = 7;
+    short b8 = 8;
+    short b9 = 9;
+    short b10 = 10;
+
+    cout << "b1 = " << b1 << " | тип: short, размер: 2б \n";
+    cout << "b2 = " << b2 << " | тип: short, размер: 2б \n";
+    cout << "b3 = " << b3 << " | тип: short, размер: 2б \n";
+    cout << "b4 = " << b4 << " | тип: short, размер: 2б \n";
+    cout << "b5 = " << b5 << " | тип: short, размер: 2б \n";
+    cout << "b6 = " << b6 << " | тип: short, размер: 2б \n";
+    cout << "b7 = " << b7 << " | тип: short, размер: 2б \n";
+    cout << "b8 = " << b8 << " | тип: short, размер: 2б \n";
+    cout << "b9 = " << b9 << " | тип: short, размер: 2б \n";
+    cout << "b10 = " << b10 << " | тип: short, размер: 2б \n";
+
+    cout << "\n";
+
+    long c1 = 1;
+    long c2 = 2;
+    long c3 = 3;
+    long c4 = 4;
+    long c5 = 5;
+    long c6 = 6;
+    long c7 = 7;
+    long c8 = 8;
+    long c9 = 9;
+    long c10 = 10;
+
+    cout << "c1 = " << c1 << " | тип: long, размер: 8б \n";
+    cout << "c2 = " << c2 << " | тип: long, размер: 8б \n";
+    cout << "c3 = " << c3 << " | тип: long, размер: 8б \n";
+    cout << "c4 = " << c4 << " | тип: long, размер: 8б \n";
+    cout << "c5 = " << c5 << " | тип: long, размер: 8б \n";
+    cout << "c6 = " << c6 << " | тип: long, размер: 8б \n";
+    cout << "c7 = " << c7 << " | тип: long, размер: 8б \n";
+    cout << "c8 = " << c8 << " | тип: long, размер: 8б \n";
+    cout << "c9 = " << c9 << " | тип: long, размер: 8б \n";
+    cout << "c10 = " << c10 << " | тип: long, размер: 8б \n";
+
+    cout << "\n";
+
+    float d1 = 1.1;
+    float d2 = 1.2;
+    float d3 = 1.3;
+    float d4 = 1.4;
+    float d5 = 1.5;
+    float d6 = 1.6;
+    float d7 = 1.7;
+    float d8 = 1.8;
+    float d9 = 1.9;
+    float d10 = 1.11;
+
+    cout << "d1 = " << d1 << " | тип: float, размер: 4б \n";
+    cout << "d2 = " << d2 << " | тип: float, размер: 4б \n";
+    cout << "d3 = " << d3 << " | тип: float, размер: 4б \n";
+    cout << "d4 = " << d4 << " | тип: float, размер: 4б \n";
+    cout << "d5 = " << d5 << " | тип: float, размер: 4б \n";
+    cout << "d6 = " << d6 << " | тип: float, размер: 4б \n";
+    cout << "d7 = " << d7 << " | тип: float, размер: 4б \n";
+    cout << "d8 = " << d8 << " | тип: float, размер: 4б \n";
+    cout << "d9 = " << d9 << " | тип: float, размер: 4б \n";
+    cout << "d10 = " << d10 << " | тип: float, размер: 4б \n";
+
+    cout << "\n";
+
+    double e1 = 1.11;
+    double e2 = 1.22;
+    double e3 = 1.33;
+    double e4 = 1.44;
+    double e5 = 1.55;
+    double e6 = 1.66;
+    double e7 = 1.77;
+    double e8 = 1.88;
+    double e9 = 1.99;
+    double e10 = 1.63;
+
+    cout << "e1 = " << e1 << " | тип: double, размер: 8б \n";
+    cout << "e2 = " << e2 << " | тип: double, размер: 8б \n";
+    cout << "e3 = " << e3 << " | тип: double, размер: 8б \n";
+    cout << "e4 = " << e4 << " | тип: double, размер: 8б \n";
+    cout << "e5 = " << e5 << " | тип: double, размер: 8б \n";
+    cout << "e6 = " << e6 << " | тип: double, размер: 8б \n";
+    cout << "e7 = " << e7 << " | тип: double, размер: 8б \n";
+    cout << "e8 = " << e8 << " | тип: double, размер: 8б \n";
+    cout << "e9 = " << e9 << " | тип: double, размер: 8б \n";
+    cout << "e10 = " << e10 << " | тип: double, размер: 8б \n";
+
+    cout << "\n";
+
+    char f1 = '!';
+    char f2 = '@';
+    char f3 = '"';
+    char f4 = '#';
+    char f5 = ';';
+    char f6 = '%';
+    char f7 = '^';
+    char f8 = '&';
+    char f9 = '*';
+    char f10 = '(';
+
+    cout << "f1 = " << f1 << " | тип: char, размер: 1б \n";
+    cout << "f2 = " << f2 << " | тип: char, размер: 1б \n";
+    cout << "f3 = " << f3 << " | тип: char, размер: 1б \n";
+    cout << "f4 = " << f4 << " | тип: char, размер: 1б \n";
+    cout << "f5 = " << f5 << " | тип: char, размер: 1б \n";
+    cout << "f6 = " << f6 << " | тип: char, размер: 1б \n";
+    cout << "f7 = " << f7 << " | тип: char, размер: 1б \n";
+    cout << "f8 = " << f8 << " | тип: char, размер: 1б \n";
+    cout << "f9 = " << f9 << " | тип: char, размер: 1б \n";
+    cout << "f10 = " << f10 << " | тип: char, размер: 1б \n";
+
+    cout << "\n";
+
+    bool g1 = false;
+    bool g2 = false;
+    bool g3 = false;
+    bool g4 = false;
+    bool g5 = false;
+    bool g6 = true;
+    bool g7 = true;
+    bool g8 = true;
+    bool g9 = true;
+    bool g10 = true;
+
+    cout << "g1 = " << g1 << " | тип: bool, размер: 1б \n";
+    cout << "g2 = " << g2 << " | тип: bool, размер: 1б \n";
+    cout << "g3 = " << g3 << " | тип: bool, размер: 1б \n";
+    cout << "g4 = " << g4 << " | тип: bool, размер: 1б \n";
+    cout << "g5 = " << g5 << " | тип: bool, размер: 1б \n";
+    cout << "g6 = " << g6 << " | тип: bool, размер: 1б \n";
+    cout << "g7 = " << g7 << " | тип: bool, размер: 1б \n";
+    cout << "g8 = " << g8 << " | тип: bool, размер: 1б \n";
+    cout << "g9 = " << g9 << " | тип: bool, размер: 1б \n";
+    cout << "g10 = " << g10 << " | тип: bool, размер: 1б \n";
+
+    int _; cin >> _;
+    return 0;
+
+}
---
+++ Работа 14/Работа 14.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+
+    const int size = 10;
+
+    int arr1[size] = { 1,2,3,4,5,6,7,8,9,10 };
+    cout << "Вывод первого массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr1[" << i + 1 << "] = " << arr1[i] << "\n";
+    }
+    cout << "\n";
+
+    short arr2[size] = { 11,22,33,44,55,66,77,88,99,101 };
+    cout << "Вывод второго массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr2[" << i + 1 << "] = " << arr2[i] << "\n";
+    }
+    cout << "\n";
+
+    long arr3[size] = { 111,222,333,444,555,666,777,888,999,1010 };
+    cout << "Вывод третьего массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr3[" << i + 1 << "] = " << arr3[i] << "\n";
+    }
+    cout << "\n";
+
+    float arr4[size] = { 11.1,22.2,33.3,44.4,55.5,66.6,77.7,88.8,99.9,10.1 };
+    cout << "Вывод четвертого массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr4[" << i + 1 << "] = " << arr4[i] << "\n";
+    }
+    cout << "\n";
+
+    double arr5[size] = { 1.11,2.22,3.33,4.44,5.55,6.66,7.77,8.88,9.99,10.11 };
+    cout << "Вывод пятого массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr5[" << i + 1 << "] = " << arr5[i] << "\n";
+    }
+    cout << "\n";
+
+    char arr6[size] = { '!','@','#','$','%','^','&','*','(',')' };
+    cout << "Вывод шестого массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr6[" << i + 1 << "] = " << arr6[i] << "\n";
+    }
+    cout << "\n";
+
+    bool arr7[size] = { true,true,true,true,true,false,false,false,false,false };
+    cout << "Вывод седьмого массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr7[" << i + 1 << "] = " << arr7[i] << "\n";
+    }
+    cout << "\n";
+
+    string arr8[size] = { "a","s","d","f","g","h","j","k","l","q" };
+    cout << "Вывод шестого массива: \n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "[+] arr8[" << i + 1 << "] = " << arr8[i] << "\n";
+    }
+    cout << "\n";
+
+
+
+    int _; cin >> _;
+    return 0;
+}
